295dc84412e7f63d5c691b2e53e2815ed91be50c
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-02-05 12:29:22 -0300

    [Desafio 9] Divisão de Valores

---
+++ math/DivisaoValores.java
@@ -0,0 +1,24 @@
+package math;
+
+import java.util.Scanner;
+
+public class DivisaoValores {
+    public static void main(String[] args) {
+        Scanner digita = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int x = digita.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int y = digita.nextInt();
+
+        if(y == 0){
+            while (y==0) {
+                System.out.println("Número inválido");
+                System.out.println("Digite o segundo número: ");
+                y = digita.nextInt();
+            }
+        }
+        int remainder = x % y;
+        System.out.println("O resto da divisão entre " + x + " e " + y + " é " + remainder);
+        digita.close();
+    }
+}
