32dc55183cccd3fca34a7f345d8829d30ca7b2d0
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-02-05 14:07:49 -0300

    [Desafio 11] Bloqueio de Usuário

--- bloqueio/BloqueioUsuario.java
+++ bloqueio/BloqueioUsuario.java
@@ -4,35 +4,36 @@ import java.util.Scanner;
 
 public class BloqueioUsuario {
     public static void main(String[] args) {
-        Scanner digita = new Scanner(System.in);
-        int count = 0;
-        String lastUser = "";
-        while (true) {
-            System.out.println("Digite seu usuário: ");
-            String usuario = digita.nextLine();
-            System.out.println("Digite sua senha: ");
-            String senha = digita.nextLine();
+        try (Scanner digita = new Scanner(System.in)) {
+            int count = 0;
+            String lastUser = "";
+            while (true) {
+                System.out.println("Digite seu usuário: ");
+                String usuario = digita.nextLine();
+                System.out.println("Digite sua senha: ");
+                String senha = digita.nextLine();
 
-            if (!usuario.equals(lastUser)){
-                count = 0;
-                lastUser = usuario;
-            }
+                if (!usuario.equals(lastUser)){
+                    count = 0;
+                    lastUser = usuario;
+                }
 
-            if(!usuario.equals("aluno")){
-                System.out.println("Usuário incorreto ou inexistente!");
-            }
-            else{
-                if(usuario.equals("aluno") && senha.equals("segredo")){
-                    System.out.println("Bem vindo!");
-                    break;
+                if(!usuario.equals("aluno")){
+                    System.out.println("Usuário incorreto ou inexistente!");
                 }
                 else{
-                    if(count == 2){
-                        System.out.println("Usuário bloqueado!");
+                    if(usuario.equals("aluno") && senha.equals("segredo")){
+                        System.out.println("Bem vindo!");
                         break;
                     }
-                    count  = count + 1;
-                    System.out.println("Senha incorreta, mais " + (3-count) + " tentivas o usuário será bloqueado!");
+                    else{
+                        if(count == 2){
+                            System.out.println("Usuário bloqueado!");
+                            break;
+                        }
+                        count  = count + 1;
+                        System.out.println("Senha incorreta, mais " + (3-count) + " tentivas o usuário será bloqueado!");
+                    }
                 }
             }
         }
