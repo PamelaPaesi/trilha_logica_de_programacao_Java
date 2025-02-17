0892d784b4797e1dc86e8b6d6177784d6b4953d8
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-02-17 16:00:22 -0300

    [Desafio 2] Mercado - Compra com Desconto

--- mercado/DescontoMercado.java
+++ mercado/DescontoMercado.java
@@ -8,33 +8,34 @@ public class DescontoMercado {
         while (sair == 1) {
             float total = 0;
 
-            Scanner digita = new Scanner(System.in);
-            System.out.println("Digite o nome do produto: ");
-            String produto = digita.nextLine();
-            System.out.println("Digite o preço do " + produto + ":");
-            float preco = digita.nextFloat();
-            System.out.println("Digite a quantidade do " + produto + ":");
-            float quant = digita.nextFloat();
-            total = preco * quant;
+            try (Scanner digita = new Scanner(System.in)) {
+                System.out.println("Digite o nome do produto: ");
+                String produto = digita.nextLine();
+                System.out.println("Digite o preço do " + produto + ":");
+                float preco = digita.nextFloat();
+                System.out.println("Digite a quantidade do " + produto + ":");
+                float quant = digita.nextFloat();
+                total = preco * quant;
 
-            float percent = 1;
+                float percent = 1;
 
-            if(quant <= 11){
-                percent = (float) (percent - 0.10);
-            }
-            else{
-                if(quant <=50){
-                    percent = (float) (percent - 0.20);
+                if(quant <= 11){
+                    percent = (float) (percent - 0.10);
                 }
                 else{
-                    percent = (float) (percent - 0.25);
+                    if(quant <=50){
+                        percent = (float) (percent - 0.20);
+                    }
+                    else{
+                        percent = (float) (percent - 0.25);
+                    }
                 }
+                total = total * percent;
+                System.out.printf("Total: %.2f\n ", total);
+                System.out.println(" --------------------------------------------- ");
+                System.out.println("Deseja sair? 0 - Sim, 1 - Não : ");
+                sair = digita.nextInt();
             }
-            total = total * percent;
-            System.out.printf("Total: %.2f\n ", total);
-            System.out.println(" --------------------------------------------- ");
-            System.out.println("Deseja sair? 0 - Sim, 1 - Não : ");
-            sair = digita.nextInt();
         }
     }
 }
