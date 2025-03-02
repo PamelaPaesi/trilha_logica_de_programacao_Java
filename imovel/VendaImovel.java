2db1e4f3ab4251cb1e958e21f96c866a20051ace
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-03-02 14:31:21 -0300

    [Desafio 5] Venda Imóvel

--- imovel/VendaImovel.java
+++ imovel/VendaImovel.java
@@ -4,36 +4,34 @@ import java.util.Scanner;
 
 public class VendaImovel {
     public static void main(String[] args) {
-        int sair = 0;
-        while (sair == 0) {
-            Double total = 0.0;
+        int sair = 1;
+        Scanner digita = new Scanner(System.in);
+        while (sair == 1) {
+            double total = 0.0;
+            System.out.println("Digite o nome do vendedor: ");
+            String nome = digita.nextLine();
+            System.out.println("Digite o valor do imóvel: ");
+            float valor = digita.nextFloat();
 
-            try (Scanner digita = new Scanner(System.in)) {
-                System.out.println("Digite o nome do vendedor: ");
-                String nome = digita.nextLine();
-                System.out.println("Digite o valor do imóvel: ");
-                float valor = digita.nextFloat();
+            double percent = 1.0;
 
-                Double percent = 1.0;
-
-                if(valor >= 50000){
-                    percent = 0.20;
+            if(valor >= 50000){
+                percent = 0.20;
+            }
+            else{
+                if(valor >= 30000){
+                    percent = 0.15;
                 }
                 else{
-                    if(valor >= 30000){
-                        percent = 0.15;
-                    }
-                    else{
-                        percent = 0.10;
-                    }
+                    percent = 0.10;
                 }
-                total = valor * percent;
-                System.out.printf("Vendedor: " + nome + " Valor do Imóvel: " + valor + " Valor da comissão %.2f\n", total);
-                System.out.println(" --------------------------------------------- ");
-                System.out.println("Deseja sair? 0 - Sim, 1 - Não : ");
-                sair = digita.nextInt();
             }
-            break;
+            total = valor * percent;
+            System.out.printf("Vendedor: " + nome + " Valor do Imóvel: " + valor + " Valor da comissão %.2f\n", total);
+            System.out.println(" --------------------------------------------- ");
+            System.out.println("Deseja sair? 0 - Sim, 1 - Não : ");
+            sair = digita.nextInt();
+            digita.nextLine();
         }
     }
 }
