f151a69a96738fd056993aa6fb974ee7b9c4aa03
Pâmela Bieger <pamela.bieger@e-unicred.com.br>
2025-03-10 16:34:03 -0300

    [Desafio 11] Bloqueio de Usuário

--- bloqueio/BloqueioUsuario.java
+++ bloqueio/BloqueioUsuario.java
@@ -6,21 +6,19 @@ public class BloqueioUsuario {
     public static void main(String[] args) {
         try (Scanner digita = new Scanner(System.in)) {
             int count = 0;
-            String lastUser = "";
             while (count < 3) {
                 System.out.println("Digite seu usuário: ");
                 String usuario = digita.nextLine();
                 System.out.println("Digite sua senha: ");
                 String senha = digita.nextLine();
-                if (!usuario.equals(lastUser)){
-                    count = 0;
-                    lastUser = usuario;
-                }
                 if(usuario.equals("aluno") && senha.equals("segredo")){
                     System.out.println("Bem vindo!");
                     break;
                 }
                 else{
+                    if(!usuario.equals("aluno")){
+                        System.out.println("Usuário inválido!");
+                    }
                     if(count == 2){
                         System.out.println("Usuário bloqueado!");
                         break;
